a10cd938d2e282a5515d04c528a19708119a3014
SeiyaYamaoka <seiya.soya.9529yaya@gmail.com>
2020-11-30 11:42:06 +0900

    2020/11/30

---
+++ src/abc153/d.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const long long LINF = 1LL << 60;
+const long long LMINF = 1LL << 63;
+const int INF = 1 << 30;
+
+#define REP(i,n) for(int i = 0;i < n;++i)
+#define REPS(i,x) for(int i = 1;i <= (int)(x);i++)
+#define RREP(i,x) for(int i=((int)(x)-1);i >= 0;i--)
+#define RREPS(i,x) for(int i=((int)(x));i > 0;i--)
+#define PB push_back
+#define F first
+#define S second
+#define MP make_pair
+#define ALL(obj) (obj).begin(),(obj).end()
+#define debug(x) cerr << #x << ": " << x << "\n"
+
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b; return true;} return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b; return true;} return false;}
+
+
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);
+    ll h,cnt = 1,ans = 0,ki = 1;
+    cin >> h;
+
+    ll eh = 1;
+    while(h > 1){
+        h /= 2;
+        cnt++;
+    }
+    for(int i = 0;i<cnt;i++){
+        ans += ki;
+        ki *= 2;  
+    }
+    cout << ans << endl;
+    return 0;
+}
