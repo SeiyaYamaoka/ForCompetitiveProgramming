221428f0f78129b766ba9fa61662e9e1ad6cc538
SeiyaYamaoka <seiya.soya.9529yaya@gmail.com>
2021-04-03 10:59:56 +0900

    2021/04/03

---
+++ src/agc002/a.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const long long LINF = 1LL << 60;
+const long long LMINF = 1LL << 63;
+const int INF = 1 << 30;
+
+#define REP(i,n) for(int i = 0;i < n;++i)
+#define REPS(i,x) for(int i = 1;i <= (int)(x);i++)
+#define RREP(i,x) for(int i=((int)(x)-1);i >= 0;i--)
+#define RREPS(i,x) for(int i=((int)(x));i > 0;i--)
+#define PB push_back
+#define F first
+#define S second
+#define MP make_pair
+#define ALL(obj) (obj).begin(),(obj).end()
+#define debug(x) cout << #x << ": " << x << "\n"
+#define out(x) cout << x << endl
+
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b; return true;} return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b; return true;} return false;}
+
+
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);
+    int a,b;
+    cin >> a >> b;
+
+    if(a >= 1){
+        out("Positive");
+    }else if(b >= 0 && a <= 0){
+        out("Zero");
+    }else if(b <= -1){
+
+        if((abs(b) - abs(a) + 1) % 2 == 0){
+            out("Positive");
+        }else{
+            out("Negative");
+        }
+    }
+
+    return 0;
+}
